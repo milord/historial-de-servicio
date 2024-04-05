3e71a923ed5fa76de0221121c9de20fbab2eaaad
milord <ermilod@yahoo.com>
2024-04-04 20:19:42 -0600

    PHP con HTML

--- index.php
+++ index.php
@@ -1,14 +1,5 @@
 <?php
-
-function dateDifference($date1, $date2) {
-    $datetime1 = new DateTime($date1);
-    $datetime2 = new DateTime($date2);
-    $interval = $datetime1->diff($datetime2);
-    return array('years' => $interval->y, 'months' => $interval->m, 'days' => $interval->d);
-}
-
-$result = dateDifference('2000-01-01', '2020-12-31');
-print_r($result);  // Outputs: Array ( [years] => 20 [months] => 11 [days] => 30 )
+$message = '';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $date1 = $_POST['date1'];
@@ -20,10 +11,26 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     if ($datetime1 && $datetime1->format('Y-m-d') === $date1 && $datetime2 && $datetime2->format('Y-m-d') === $date2) {
         $result = dateDifference($date1, $date2);
-        echo $result;
+        $message = "The difference between the two dates is: " . $result;
     } else {
-        echo "Invalid date format. Please enter dates in the format 'YYYY-MM-DD'.";
+        $message = "Invalid date format. Please enter dates in the format 'YYYY-MM-DD'.";
     }
 }
 
+function dateDifference($date1, $date2) {
+    $datetime1 = new DateTime($date1);
+    $datetime2 = new DateTime($date2);
+    $interval = $datetime1->diff($datetime2);
+    return sprintf('%d years, %d months, %d days', $interval->y, $interval->m, $interval->d);
+}
 ?>
+
+<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+    <label for="date1">Date 1:</label><br>
+    <input type="date" id="date1" name="date1"><br>
+    <label for="date2">Date 2:</label><br>
+    <input type="date" id="date2" name="date2"><br>
+    <input type="submit" value="Submit">
+</form>
+
+<p><?php echo $message; ?></p>
